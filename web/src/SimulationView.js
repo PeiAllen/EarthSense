224f41db88ae31ee17c963316188c03a0dd87947
Allen <peiall@utschools.ca>
2019-12-14 17:31:35 -0500

    simulation complete(except for preset points)

--- web/src/SimulationView.js
+++ web/src/SimulationView.js
@@ -1,23 +1,53 @@
+/* global google */
 import React from 'react';
+import GoogleMapReact from 'google-map-react';
 
-import {  MenuList, Drawer, MenuItem, Grid, Card, CardActionArea, CardContent, Typography, CardActions, Button, AppBar, Toolbar, IconButton} from '@material-ui/core';
-import MenuIcon from '@material-ui/icons/Menu';
-import './App.css';
-import ClickAwayListener from '@material-ui/core/ClickAwayListener';
-import CardThing from './CardThing.js';
 
+const heatMapData={positions: [
+  ],
+  options: {   
+    radius: 10,   
+    opacity: 0.6,
+  }
+}
 class SimulationView extends React.Component {
-    constructor(props) {
-        super(props);
+    constructor(){
+      super();
+      this.MapMount=this.MapMount.bind(this);
+      this.MapClick=this.MapClick.bind(this);
+      this.MapRef=React.createRef();
+    }
+    static defaultProps = {
+        center: {
+          lat: 43.2557,
+          lng: -79.8711
+        },
+        zoom: 11
+    };
+    MapMount(){
+      this.MapRef.heatmap.data.push({location: new google.maps.LatLng(43.2557, -79.8711), weight: 1});
+    }
+    MapClick({lat,lng}) {
+      this.MapRef.heatmap.data.push({location: new google.maps.LatLng(lat, lng), weight: 1});
+    }
+    render() {
+      return (
+        <div style={{ height: '100vh', width: '100%' }}>
+          <GoogleMapReact  
+            ref={el => this.MapRef = el}  
+            defaultCenter={this.props.center}
+            defaultZoom={this.props.zoom}
+            heatmapLibrary={true}     
+            onTilesLoaded={this.MapMount}     
+            heatmap={heatMapData}
+            onClick={this.MapClick}  
+          >
+          </GoogleMapReact>
+        </div>
+      );
     }
 
-    render() {
-        return(
-            <div>
-                simulation here
-            </div>
-        )
-    }
 }
 
 export default SimulationView;
+
