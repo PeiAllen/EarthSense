5d41fed23c60cb179c7dfc1b8680dbf662332d84
jjjding2 <xiongjason2002@gmail.com>
2019-12-15 18:16:40 -0500

    Finalized NodeMCU code

--- Hardware/Arduino C Firebase Code.ino
+++ Hardware/Arduino C Firebase Code.ino
@@ -4,6 +4,10 @@
 #include <NTPClient.h>
 #include <WiFiUdp.h>
 
+#include "MQ135.h"
+
+MQ135 gasSensor = MQ135(A0);
+
 //time
 #define NTP_OFFSET   60 * 60      // In seconds
 #define NTP_INTERVAL 60 * 1000    // In miliseconds
@@ -11,11 +15,17 @@
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, NTP_ADDRESS, NTP_OFFSET, NTP_INTERVAL);
 
-#define WIFI_SSID "MIP Internet"
-#define WIFI_PASSWORD "Connect@MIP"
+//#define WIFI_SSID "MIP Internet"
+//#define WIFI_PASSWORD "Connect@MIP"
 
-//#define WIFI_SSID "poopoo"
-//#define WIFI_PASSWORD "13579abc"
+#define WIFI_SSID "blobheart2"
+#define WIFI_PASSWORD "13579abc"
+
+//#define WIFI_SSID "Sleep"
+//#define WIFI_PASSWORD "Hmmmmmmm"
+
+//#define WIFI_SSID "segment ree"
+//#define WIFI_PASSWORD "20030524"
 
 #define FIREBASE_HOST "air-quality-database.firebaseio.com"
 #define FIREBASE_AUTH "KYRszKUaiWMSDxn9K5cgBloPCjgy0dNEG36fDuWZ"
@@ -27,29 +37,62 @@ DHTesp dht;
 int value = 0;
 int vHumidity = 0;
 
+float sLoud = 0;
+float sTot = 0;
+
+int sVal = 0;
+int gVal = 0;
 
 void setup()
 {
   Serial.begin(115200);
+  pinMode (15, INPUT);
+  
   timeClient.begin();
 
   delay(2000);
   Serial.println('\n');
-  
+
+  pinMode(BUILTIN_LED, OUTPUT);
+  if(WiFi.status() == WL_CONNECTED){
+    Serial.println("Connected!");  
+  }else{
   wifiConnect();
+  }
   if(WiFi.status() == WL_CONNECTED){
     Serial.println("Connected!");  
   }
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
 
   dht.setup(5, DHTesp::DHT11);
+  
+  
 }
 
 void loop()
 {  
 
+  //setHumidity();
+
+
   setHumidity();
-  delay(1000);
+  delay(300);
+  for(int i = 0; i<50; i++){
+    noise();
+    delay(50);  
+  }
+  gas();
+
+  delay(300);
+  
+  for(int i = 0; i<50; i++){
+    noise();
+    delay(50);  
+  }
+  
+  
+
+
   /*
   timeClient.update();
 
@@ -73,6 +116,23 @@ void loop()
 */
 }
 
+void gas(){
+  timeClient.update();
+
+  float rzero = gasSensor.getRZero();
+  float ppm = gasSensor.getPPM();
+  Serial.println(ppm);
+  Firebase.pushInt("/Air/"+String(gVal), ppm);
+  Firebase.pushInt("/Air/"+String(gVal), timeClient.getEpochTime());
+  
+  if(Firebase.failed()){
+    Serial.println("failed");  
+    Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  }
+  
+  gVal ++;
+  
+}
 void setHumidity()
 {
   //delay(dht.getMinimumSamplingPeriod());
@@ -86,11 +146,13 @@ void setHumidity()
   Serial.println(val2);
 
   Firebase.pushFloat("/Humidity/"+String(value), dht.getHumidity());
+  delay(10);
   Firebase.pushInt("/Humidity/"+String(value), timeClient.getEpochTime());
-
+  delay(10);
   timeClient.update();
 
   Firebase.pushFloat("/Temperature/"+String(value), dht.getTemperature());
+  delay(10);
   Firebase.pushInt("/Temperature/"+String(value), timeClient.getEpochTime());
   
   if(Firebase.failed()){
@@ -101,6 +163,44 @@ void setHumidity()
 
   value++;
   
+}
+
+void noise()
+{
+  int statusSensor =digitalRead(15);
+
+  sTot ++;
+  if(statusSensor == 1){
+    sLoud ++;
+  }else{
+    //Serial.println("___");
+  }
+  if(sTot == 100){
+    timeClient.update();
+    int level = 0;
+    Serial.println("Loudness value");
+    Serial.println(sLoud/sTot);
+    if(sLoud/sTot <= 0.03){
+      level = 0;
+    }else if(sLoud/sTot <= 0.06){
+      level = 1;
+    }else if(sLoud/sTot <= 0.1){
+      level = 2;  
+    }else{
+      level = 3;  
+    }
+    Firebase.pushInt("/Noise/"+String(sVal), level);
+    Firebase.pushInt("/Noise/"+String(sVal), timeClient.getEpochTime());
+    if(Firebase.failed()){
+      Serial.println("failed");  
+      Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+
+    }
+    sVal ++;
+    sLoud = 0;
+    sTot = 0;
+   
+  }
 }
 
 void wifiConnect()
